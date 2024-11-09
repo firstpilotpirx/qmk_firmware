c9c6551f53b4d948ed5e875b7299c26c0ad9a2fd
Alexandr <a.volkov@1inch.io>
2024-11-09 13:25:19 +0100

    chore: wip on key repeat

--- keyboards/handwired/dactyl_manuform/5x6_5_ergohaven/keymaps/home_row_3x5/config.h
+++ keyboards/handwired/dactyl_manuform/5x6_5_ergohaven/keymaps/home_row_3x5/config.h
@@ -22,14 +22,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KINETIC_SPEED
 
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
 #define RETRO_TAPPING
 #define PERMISSIVE_HOLD
 
-#define AUTO_SHIFT_TIMEOUT 200
-#define RETRO_SHIFT
-// #define NO_ACTION_TAPPING
-// #define HOLD_ON_OTHER_KEY_PRESS
-
-
-// #define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define AUTO_SHIFT_TIMEOUT 175
+#define AUTO_SHIFT_REPEAT
+#define RETRO_SHIFT 500
